65753bde451e1ae9fe11219eba96e19432a27b15
Joucke Hempenius <joucke@gmail.com>
2013-10-16 15:18:34 +0200

    Fixes

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,11 +1,11 @@
 class RequestsController < ApplicationController
   def index
     # Get all requests
-    @request = Request.all
+    @requests = Request.all
   end
 
   def show
-    redirect_to :action => 'index' # Redirect to index function
+    # No view available (yet)
   end
 
   def new
@@ -15,18 +15,15 @@ class RequestsController < ApplicationController
   def create
     # Store new request into the database
     @request = Request.new
-    @request.user = User.find_by_name("John Doe") # Placeholder
+    @request.user = User.first
     @request.title = params[:title]
     @request.date = params[:date]
 
-    respond_to do |format|
-      if @request.save
-        format.html { redirect_to @request, notice: 'User was successfully created.' }
-        format.json { render action: 'index', status: :created, location: @request }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @request.errors, status: :unprocessable_entity }
-      end
+    if @request.save
+      # Redirect to index function on success
+      redirect_to :action => 'index', notice: 'User was successfully created.'
+    else
+      render action: 'new'
     end
   end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -3,4 +3,11 @@ class Request < ActiveRecord::Base
 
   validates :title, :presence => true, :length => { :minimum => 10 }
   validates :date, :presence => true
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+	if date.present? && date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
 end
