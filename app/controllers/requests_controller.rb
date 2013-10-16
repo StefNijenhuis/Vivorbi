2350aff922134995fca85100b996289826183037
Bullace <bullace@gmail.com>
2013-10-16 10:50:06 +0200

    Requests are saved into database

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,9 +3,15 @@ class RequestsController < ApplicationController
   end
 
   def new
+    @request = Request.new
   end
 
   def create
-    render text: "Title: " + params[:title] + "<br>Date: " + params[:date]
+    #render text: "Title: " + params[:title] + "<br>Date: " + params[:date]
+    request = Request.new
+    request.title = params[:title]
+    request.date = params[:date]
+    request.save
+
   end
 end
