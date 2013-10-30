ac0b7eb512b27f7873c8877c9da8ae61a2dcf01d
Bullace <bullace@gmail.com>
2013-10-30 12:45:33 +0100

    Added new migration that will add new fields to the users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create([{ name: 'John Doe' }])
+User.create([{
+ first_name: 'NHL',
+ last_name: 'Hogeschool',
+ street: 'Rengerslaan',
+ house_number: '10',
+ postal_code: '8917DD',
+ place: 'Leeuwarden',
+ date_of_birth: '2009-09-1',
+ phone: '0582512345',
+ email: 'info@nhl.nl',
+ hobbies: 'Being a building'
+}])
