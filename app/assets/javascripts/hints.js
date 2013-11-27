4fa9ca82807c5eeaa75da35933f58a5535d95049
johman10 <johan@rc14033.lan.nhl.nl>
2013-11-27 14:35:32 +0100

    Fixed overflow of hint

--- app/assets/javascripts/hints.js
+++ app/assets/javascripts/hints.js
@@ -4,40 +4,11 @@ $(document).ready(function() {
 
 		$(this).parent().siblings('.hint_wrapper').addClass('visible_hint');
 
-		adjustHeight();
 
 	}).blur(function() {
 
 		$('.visible_hint').removeClass('visible_hint');
-		restoreHeight();
 
 	});
 
-	$(window).resize(adjustHeight);
-
 });
-
-function adjustHeight() {
-
-	if($(window).width() >= 768) {
-
-		var fieldHeight = $('.visible_hint').siblings('.message_wrapper').height();
-		var hintHeight = $('.visible_hint').height();
-		if(hintHeight > fieldHeight) {
-
-			$('.visible_hint').siblings('.message_wrapper').height($('.visible_hint').height());
-		}
-	}
-	else {
-
-		restoreHeight();
-		
-	}
-
-}
-
-function restoreHeight() {
-
-	$('.message_wrapper').height('auto');
-
-}
