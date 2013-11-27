4a0c4c59b63312f64cfde6f752b5ed6142a71b85
Jan-Jurjen Lemstra <janjurjen.lemstra@gmail.com>
2013-11-27 13:47:55 +0100

    Hints jquery

--- app/assets/javascripts/hints.js
+++ app/assets/javascripts/hints.js
@@ -1,21 +1,43 @@
 $(document).ready(function() {
 
-	$('#message_form_title').click(function() {
+	$('.show_hint').focus(function() {
 
-		$('#hint_wrapper_2').hide(1000);
+		$(this).parent().siblings('.hint_wrapper').addClass('visible_hint');
 
-		$('#hint_wrapper_1').show(1000);
+		adjustHeight();
+
+	}).blur(function() {
+
+		$('.visible_hint').removeClass('visible_hint');
+		restoreHeight();
 
 	});
 
-	$('#message_form_description').click(function() {
-
-		$('#hint_wrapper_1').hide(1000);
-
-		$('#hint_wrapper_2').show(1000);
-		
-	});
-
-
+	$(window).resize(adjustHeight);
 
 });
+
+function adjustHeight() {
+
+	if($(window).width() >= 768) {
+
+		var fieldHeight = $('.visible_hint').siblings('.message_wrapper').height();
+		var hintHeight = $('.visible_hint').height();
+		if(hintHeight > fieldHeight) {
+
+			$('.visible_hint').siblings('.message_wrapper').height($('.visible_hint').height());
+		}
+	}
+	else {
+
+		restoreHeight();
+		
+	}
+
+}
+
+function restoreHeight() {
+
+	$('.message_wrapper').height('auto');
+
+}
