b2853dd4d47b5f1c4909e056f65d3be1938c8371
Jan-Jelles van Stormbroek <bullace@gmail.com>
2013-12-03 12:42:49 +0100

    Home page search methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
   def home
-  	
+    @user = User.first
+    @last = Message.last(3)
+    @closest = Message.find_by_location_and_radius(@user.latitude,@user.longitude,999,3)
+    @popularity = Message.popularity(3)
+
+    @row = [
+      [@last[0], @closest[0], @popularity[0]],
+      [@last[1], @closest[1], @popularity[1]],
+      [@last[2], @closest[2], @popularity[2]]
+    ]
+
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -4,11 +4,19 @@ class Message < ActiveRecord::Base
 
   validates_presence_of :title, :body
 
+  def self.popularity(limit = 10, offset = 0, order_count = "DESC", order_create = "DESC")
+    self.all(:joins => :comments,
+             :group => 'messages.id',
+             :order => "count(*) #{order_count}, created_at #{order_create}",
+             :limit => limit,
+             :offset => offset)
+  end
+
   def distance
     read_attribute(:distance).to_f
   end
 
-  def self.find_by_location_and_radius(s_lat,s_long,radius)
-    self.joins(:user).select("messages.*, 6371 * acos( cos( radians( #{s_lat} ) ) * cos( radians( users.latitude ) ) * cos( radians( users.longitude ) - radians( #{s_long} ) ) + sin( radians( #{s_lat} ) ) * sin( radians( users.latitude ) ) ) AS distance").where("6371 * acos( cos( radians( #{s_lat} ) ) * cos( radians( users.latitude ) ) * cos( radians( users.longitude ) - radians( #{s_long} ) ) + sin( radians( #{s_lat} ) ) * sin( radians( users.latitude ) ) ) <= #{radius}")
+  def self.find_by_location_and_radius(s_lat,s_long,radius,limit = 999)
+    self.joins(:user).select("messages.*, 6371 * acos( cos( radians( #{s_lat} ) ) * cos( radians( users.latitude ) ) * cos( radians( users.longitude ) - radians( #{s_long} ) ) + sin( radians( #{s_lat} ) ) * sin( radians( users.latitude ) ) ) AS distance").where("6371 * acos( cos( radians( #{s_lat} ) ) * cos( radians( users.latitude ) ) * cos( radians( users.longitude ) - radians( #{s_long} ) ) + sin( radians( #{s_lat} ) ) * sin( radians( users.latitude ) ) ) <= #{radius}").order("distance").limit(limit)
   end
 end
