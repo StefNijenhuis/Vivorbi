a1242337dba6e0699ff6943b639dea1ee9ea4b37
Joucke Hempenius <joucke@gmail.com>
2013-12-04 14:03:20 +0100

    fix for location find

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   def home
     amount = 3
     last = Message.last(amount)
-    nearby = Message.find_by_location_and_radius(@user.latitude,@user.longitude,999,amount)
+    location = {'latitude'=>@user.latitude,'longitude'=>@user.longitude}
+    nearby = Message.find_by_location_and_radius(location,999,amount)
     popular = Message.find_by_popularity(amount)
     @columns = [last, nearby, popular]
     @columnTitles = ["Recent", "In de buurt", "Populair"]
