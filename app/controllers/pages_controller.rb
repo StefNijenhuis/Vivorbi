f50deec7b3074ef6e7f638a4e65054eee8203f20
Jan-Jelles van Stormbroek <bullace@gmail.com>
2013-12-18 12:30:46 +0100

    .all fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     amount = 3
-    last = Message.all(:order => "created_at DESC", :limit => amount)
+    last = Message.all.order("created_at DESC").limit(amount)
     location = {'latitude'=>@user.latitude,'longitude'=>@user.longitude}
     nearby = Message.find_by_location_and_radius(location,999).limit(3)
     popular = Message.find_by_popularity(amount)
