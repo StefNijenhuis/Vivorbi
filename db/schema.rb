5130ba74c88a7e2061c28fa6669e0be7bb8b22aa
KLaas <klaas.88@gmail.com>
2013-12-19 13:27:08 +0100

    Changed category colors again

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131219094221) do
+ActiveRecord::Schema.define(version: 20131209141856) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "categories", force: true do |t|
     t.string   "title"
